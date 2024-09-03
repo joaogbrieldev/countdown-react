266a75835bccac990703a37b5dc586b6caa3d7f5
João Figueiredo <121825554+joaogbrieldev@users.noreply.github.com>
2024-09-02 21:36:49 -0300

    Develop (#3)
    * feat: initial player component
    
      understanding the concept of useRef vs useState
    
    * feat: new component to create a challengers (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import Player from "./components/Player.tsx";
+import TimerChallenger from "./components/TimerChallenger.tsx";
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenger targetTime={1} title="Easy" />
+        <TimerChallenger targetTime={5} title="Not easy" />
+        <TimerChallenger targetTime={10} title="Getting tough" />
+        <TimerChallenger targetTime={15} title="Pros only" />
+      </div>
     </>
   );
 }
