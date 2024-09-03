61559ee5c384a97382f293ad0acca501eef13425
João Figueiredo <121825554+joaogbrieldev@users.noreply.github.com>
2024-09-03 08:21:50 -0300

    Feature/timer challenger component (#4)
    * feat: new component to create a challengers
    
    * initial setup timer challenger component fisish

--- src/components/TimerChallenger.tsx
+++ src/components/TimerChallenger.tsx
@@ -1,23 +1,35 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 type TimerChallengerParam = {
   title: string;
   targetTime: number;
 };
 
+type Ref = {
+  current: number | null | void;
+};
+
 export default function TimerChallenger({
   title,
   targetTime,
 }: TimerChallengerParam) {
+  const timer: Ref = useRef();
   const [timerExpired, setTimerExpired] = useState(false);
   const [timerStarted, setTimerStarted] = useState(false);
 
   function handleStart() {
     setTimeout(() => {
-      setTimerExpired(true);
+      timer.current = setTimerExpired(true);
       setTimerStarted(false);
     }, targetTime * 1000);
     setTimerStarted(true);
+  }
+
+  function handleStop() {
+    if (timer.current) {
+      clearTimeout(timer.current);
+      timer.current = null;
+    }
   }
 
   return (
@@ -28,7 +40,7 @@ export default function TimerChallenger({
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button onClick={handleStart}>
+        <button onClick={timerStarted ? handleStop : handleStart}>
           {timerStarted ? "Stop" : "Start"} Challenger
         </button>
       </p>
